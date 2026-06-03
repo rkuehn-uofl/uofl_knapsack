05643146c660ed41a1fa24c1b675b5065e4e91a0
Randy Kuehn <randy.kuehn@louisville.edu>
2026-06-03 17:24:24 +0000

    Update application controller decorator logout

--- app/controllers/application_controller_decorator.rb
+++ app/controllers/application_controller_decorator.rb
@@ -6,6 +6,12 @@ module ApplicationControllerDecorator
     base.include TurnstileProtectedPublicPage
     base.prepend_before_action :require_turnstile_for_public_page
   end
+
+  protected
+
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path(locale: params[:locale].presence)
+  end
 end
 
 ApplicationController.prepend(ApplicationControllerDecorator)
